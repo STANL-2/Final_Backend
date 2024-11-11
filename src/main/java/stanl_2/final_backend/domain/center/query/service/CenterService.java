a4b62011e935110477c5eb8476d3f8163eaae557
minseokKim6823 <alstjr6823@naver.com>
2024-11-11 18:02:43 +0900

    8081로 포트 번호 수정

--- src/main/java/stanl_2/final_backend/domain/center/query/service/CenterService.java
+++
@@ -1,7 +0,0 @@
-package stanl_2.final_backend.domain.center.query.service;
-
-import stanl_2.final_backend.domain.center.query.dto.CenterSelectIdDTO;
-
-public interface CenterService {
-    CenterSelectIdDTO selectByCenterId(Long centerId);
-}
