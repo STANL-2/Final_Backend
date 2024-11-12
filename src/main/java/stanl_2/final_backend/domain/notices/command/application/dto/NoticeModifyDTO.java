4249fe96d62e9eb7f08c8c1dd361ad98256e7d51
minseokKim6823 <alstjr6823@naver.com>
2024-11-12 19:06:28 +0900

    feat: 공지사항 DTO세팅 (#26)

--- src/main/java/stanl_2/final_backend/domain/notices/command/application/dto/NoticeModifyDTO.java
+++ src/main/java/stanl_2/final_backend/domain/notices/command/application/dto/NoticeModifyDTO.java
@@ -1,4 +1,14 @@
 package stanl_2.final_backend.domain.notices.command.application.dto;
 
 public class NoticeModifyDTO {
+    private String title;
+
+    private String tag;
+
+    private String classification;
+
+    private String content;
+
+    private String updatedAt;
+
 }
---
+++ src/main/java/stanl_2/final_backend/domain/notices/command/application/dto/NoticeRegistDTO.java
@@ -0,0 +1,13 @@
+package stanl_2.final_backend.domain.notices.command.application.dto;
+
+public class NoticeRegistDTO {
+    private String title;
+
+    private String tag;
+
+    private String classification;
+
+    private String content;
+
+    private String memberId;
+}
