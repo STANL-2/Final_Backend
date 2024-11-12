d7126cc2e140a222bf65187d2cd4b0865d326fd7
yuhyejin <hj_eu@naver.com>
2024-11-12 11:34:40 +0900

    refactor: 계약서 등록 수정(#21)

--- src/main/java/stanl_2/final_backend/domain/contract/command/domain/service/ContractServiceImpl.java
+++ src/main/java/stanl_2/final_backend/domain/contract/command/domain/service/ContractServiceImpl.java
@@ -22,12 +22,16 @@ public class ContractServiceImpl implements ContractService {
     @Override
     @Transactional
     public void registerContract(ContractRegistRequestDTO contractRegistRequestDTO) {
-        // 회원인지 확인여부
+        // 회원인지 확인여부 및 값 가져오기
+        
+        // 일련번호로 제품테이블의 총식별번호 찾아서 제품 가져오기
+
+        // 고객전화번호로 고객테이블 찾아서 고객이 있으면 넘어가고, 고객이 없으면 고객테이블에 고객 정보 넣기
 
         Contract contract = modelMapper.map(contractRegistRequestDTO, Contract.class);
 
-        contract.setCentId("CEN_000000001");
-        contract.setProdId("PRO_000000001");
+        contract.setCentId("CEN_000000001");    // 회원의 매장번호 넣기
+        contract.setProdId("PRO_000000001");    // 제품 번호 넣기
 
         contractRepository.save(contract);
     }
