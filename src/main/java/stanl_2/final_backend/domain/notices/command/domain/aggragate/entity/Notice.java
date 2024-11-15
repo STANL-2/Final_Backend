bf90aea0529a767c5fb94f8161e6e983832e9721
minseokKim6823 <alstjr6823@naver.com>
2024-11-15 14:28:16 +0900

    fix: active 1(true) or 0(false)로 저장

--- src/main/java/stanl_2/final_backend/domain/notices/command/domain/aggragate/entity/Notice.java
+++ src/main/java/stanl_2/final_backend/domain/notices/command/domain/aggragate/entity/Notice.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
-import stanl_2.final_backend.global.config.BooleanToStringConverterConfig;
 import stanl_2.final_backend.global.config.PrefixGeneratorConfig;
 
 import java.time.ZoneId;
@@ -51,7 +50,6 @@ public class Notice {
     private String deletedAt;
 
     @Column(name = "ACTIVE", nullable = false)
-    @Convert(converter = BooleanToStringConverterConfig.class)
     private Boolean active = true;
 
     @Column(name = "MEM_ID", nullable = false)
--- src/main/java/stanl_2/final_backend/domain/notices/query/service/NoticeServiceImpl.java
+++ src/main/java/stanl_2/final_backend/domain/notices/query/service/NoticeServiceImpl.java
@@ -5,7 +5,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import stanl_2.final_backend.domain.notices.query.dto.NoticeDTO;
+import stanl_2.final_backend.domain.notices.query.dto.SearchDTO;
 import stanl_2.final_backend.domain.notices.query.repository.NoticeMapper;
 
 import java.util.List;
@@ -20,13 +22,24 @@ public class NoticeServiceImpl implements NoticeService{
         this.noticeMapper = noticeMapper;
     }
 
-
+    @Transactional
     @Override
     public Page<NoticeDTO> findAllNotices(Pageable pageable) {
         int offset = Math.toIntExact(pageable.getOffset());
         int size = pageable.getPageSize();
         List<NoticeDTO> notices = noticeMapper.findAllNotices(offset,size);
         int totalElements = noticeMapper.findNoticeCount(); // 총 개수 조회
+
+        return new PageImpl<>(notices, pageable, totalElements);
+    }
+
+    @Transactional
+    @Override
+    public Page<NoticeDTO> findNotices(Pageable pageable, SearchDTO searchDTO) {
+        int offset = Math.toIntExact(pageable.getOffset());
+        int size = pageable.getPageSize();
+        List<NoticeDTO> notices = noticeMapper.findNotices(offset,size,searchDTO);
+        int totalElements = noticeMapper.findNoticesCount(searchDTO);
 
         return new PageImpl<>(notices, pageable, totalElements);
     }
