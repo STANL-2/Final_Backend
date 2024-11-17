9f851dd4959e92e069a45cd22b4beb2d4a8eef28
yuhyejin <hj_eu@naver.com>
2024-11-18 00:36:14 +0900

    feat: 발주서 조회 세팅 (#59)

---
+++ src/main/java/stanl_2/final_backend/domain/purchase_order/query/controller/PurchaseOrderController.java
@@ -0,0 +1,9 @@
+package stanl_2.final_backend.domain.purchase_order.query.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("PurchaseOrderQueryController")
+@RequestMapping("/api/v1/purchase-order")
+public class PurchaseOrderController {
+}
---
+++ src/main/java/stanl_2/final_backend/domain/purchase_order/query/service/PurchaseOrderQueryServiceImpl.java
@@ -0,0 +1,7 @@
+package stanl_2.final_backend.domain.purchase_order.query.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseOrderQueryServiceImpl implements PurchaseOrderQueryService {
+}
