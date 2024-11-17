40b6cb207993cb5300b9f47c0843ef2ae5634c77
Bang1999 <bangdh1593@gmail.com>
2024-11-17 15:09:16 +0900

    fix: 현재 사용자 불러오는 방법 수정(#41)

--- src/main/java/stanl_2/final_backend/domain/member/command/application/controller/MemberController.java
+++ src/main/java/stanl_2/final_backend/domain/member/command/application/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import stanl_2.final_backend.domain.member.command.application.service.MemberCommandService;
 import stanl_2.final_backend.domain.member.common.response.MemberResponseMessage;
 
+import java.security.Principal;
+
 @Slf4j
 @RestController("commandMemberController")
 @RequestMapping("/api/v1/member")
@@ -23,23 +25,37 @@ public class MemberController {
         this.memberCommandService = memberCommandService;
     }
 
+//    @GetMapping("/authorities")
+//    public ResponseEntity<MemberResponseMessage> check(Authentication authentication) {
+//        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getName())) {
+//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+//                    .body(MemberResponseMessage.builder()
+//                            .httpStatus(401)
+//                            .msg("Unauthorized")
+//                            .build());
+//        }
+//
+//        // 인증된 사용자 정보 출력
+//        log.info("인증된 사용자: {}", authentication.getName());
+//
+//        return ResponseEntity.ok(MemberResponseMessage.builder()
+//                .httpStatus(200)
+//                .msg("성공")
+//                .result("인증된 사용자: " + authentication.getName())
+//                .build());
+//    }
+
+
     @GetMapping("/authorities")
-    public ResponseEntity<MemberResponseMessage> check(Authentication authentication) {
-        if (authentication == null || !authentication.isAuthenticated() || "anonymousUser".equals(authentication.getName())) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(MemberResponseMessage.builder()
-                            .httpStatus(401)
-                            .msg("Unauthorized")
-                            .build());
-        }
+    public ResponseEntity<MemberResponseMessage> check(Principal principal) {
 
         // 인증된 사용자 정보 출력
-        log.info("인증된 사용자: {}", authentication.getName());
+        log.info("인증된 사용자: {}", principal.getName());
 
         return ResponseEntity.ok(MemberResponseMessage.builder()
                 .httpStatus(200)
                 .msg("성공")
-                .result("인증된 사용자: " + authentication.getName())
+                .result("인증된 사용자: " + principal.getName())
                 .build());
     }
 
