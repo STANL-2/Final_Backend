06e07442d3dbbd6d04e634834486dd3af1b2d364
minseokKim6823 <alstjr6823@naver.com>
2024-11-18 21:37:42 +0900

    feat: 깔끔하게 완료~! (#76)

--- src/main/java/stanl_2/final_backend/global/config/RedisConfig.java
+++ src/main/java/stanl_2/final_backend/global/config/RedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RedisConfig {
+
     private final RedisConnectionFactory redisConnectionFactory;
 
     public RedisConfig(RedisConnectionFactory redisConnectionFactory) {
