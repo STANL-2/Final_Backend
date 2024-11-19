c999ca833a989378c245ae5af05e5d7cec29243a
woosuk1 <giuseog@gmail.com>
2024-11-19 22:10:59 +0900

    fix: center 도메인 동작여부 확인 & pk 생성값 수정(#30)

--- src/main/java/stanl_2/final_backend/domain/center/command/domain/aggregate/entity/Center.java
+++ src/main/java/stanl_2/final_backend/domain/center/command/domain/aggregate/entity/Center.java
@@ -25,7 +25,7 @@ public class Center {
     @GeneratedValue(generator = "PrefixGeneratorConfig")
     @GenericGenerator(name = "PrefixGeneratorConfig",
             type = PrefixGeneratorConfig.class,
-            parameters = @org.hibernate.annotations.Parameter(name = "prefix", value = "CENT")
+            parameters = @org.hibernate.annotations.Parameter(name = "prefix", value = "CEN")
     )
     @Column(name ="CENT_ID")
     private String id;
