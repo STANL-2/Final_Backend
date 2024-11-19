52a46632e3e4611f3354f0c0c1b45f4b3462afe0
minseokKim6823 <alstjr6823@naver.com>
2024-11-19 21:04:15 +0900

    feat : 레디스 세팅 및 구현(#76)

--- src/main/java/stanl_2/final_backend/domain/notices/query/service/NoticeServiceImpl.java
+++ src/main/java/stanl_2/final_backend/domain/notices/query/service/NoticeServiceImpl.java
@@ -4,9 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import stanl_2.final_backend.domain.notices.command.domain.aggragate.entity.Notice;
 import stanl_2.final_backend.domain.notices.query.dto.NoticeDTO;
 import stanl_2.final_backend.domain.notices.query.dto.SearchDTO;
 import stanl_2.final_backend.domain.notices.query.repository.NoticeMapper;
@@ -17,10 +17,12 @@ import java.util.List;
 @Service("queryNoticeServiceImpl")
 public class NoticeServiceImpl implements NoticeService{
     private final NoticeMapper noticeMapper;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
-    public NoticeServiceImpl(NoticeMapper noticeMapper) {
+    public NoticeServiceImpl(NoticeMapper noticeMapper, RedisTemplate redisTemplate) {
         this.noticeMapper = noticeMapper;
+        this.redisTemplate = redisTemplate;
     }
 
     @Transactional
@@ -28,9 +30,19 @@ public class NoticeServiceImpl implements NoticeService{
     public Page<NoticeDTO> findAllNotices(Pageable pageable) {
         int offset = Math.toIntExact(pageable.getOffset());
         int size = pageable.getPageSize();
-        List<NoticeDTO> notices = noticeMapper.findAllNotices(offset,size);
-        int totalElements = noticeMapper.findNoticeCount(); // 총 개수 조회
+        String cacheKey = "myCache::notices::offset=" + offset + "::size=" + size;
 
+        List<NoticeDTO> notices = (List<NoticeDTO>) redisTemplate.opsForValue().get(cacheKey);
+
+        // 캐시에 데이터가 없다면 DB에서 조회하고 캐시에 저장
+        if (notices == null) {
+            System.out.println("데이터베이스에서 데이터 조회 중...");
+            notices = noticeMapper.findAllNotices(offset, size);
+            redisTemplate.opsForValue().set(cacheKey, notices);
+        } else {
+            System.out.println("캐시에서 데이터 조회 중...");
+        }
+        int totalElements = noticeMapper.findNoticeCount(); // 총 개수 조회
         return new PageImpl<>(notices, pageable, totalElements);
     }
 
