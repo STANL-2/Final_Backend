89a63b339621f1728ecf5fba25457b62d3e20536
giuseog <giuseog@gmail.com>
2024-11-21 12:25:50 +0900

    fix: 에러 처리

--- src/main/java/stanl_2/final_backend/domain/center/command/application/controller/CenterController.java
+++ src/main/java/stanl_2/final_backend/domain/center/command/application/controller/CenterController.java
@@ -8,11 +8,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import stanl_2.final_backend.domain.A_sample.common.response.SampleResponseMessage;
 import stanl_2.final_backend.domain.center.command.application.dto.request.CenterModifyRequestDTO;
 import stanl_2.final_backend.domain.center.command.application.dto.request.CenterRegistRequestDTO;
 import stanl_2.final_backend.domain.center.command.application.service.CenterCommandService;
-import stanl_2.final_backend.domain.center.common.response.ResponseMessage;
+import stanl_2.final_backend.domain.center.common.response.CenterResponseMessage;
 
 @RestController("commandCenterController")
 @RequestMapping("/api/v1/center")
@@ -28,16 +27,16 @@ public class CenterController {
     @Operation(summary = "매장 등록")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공",
-                    content = {@Content(schema = @Schema(implementation = SampleResponseMessage.class))})
+                    content = {@Content(schema = @Schema(implementation = CenterResponseMessage.class))})
     })
     @PostMapping("")
-    public ResponseEntity<?> postTest(@RequestBody CenterRegistRequestDTO centerRegistRequestDTO){
+    public ResponseEntity<CenterResponseMessage> postTest(@RequestBody CenterRegistRequestDTO centerRegistRequestDTO){
         /* 설명. memberId 토큰으로 받는 것 고려 */
 
         centerCommandService.registCenter(centerRegistRequestDTO);
 
 
-        return ResponseEntity.ok(ResponseMessage.builder()
+        return ResponseEntity.ok(CenterResponseMessage.builder()
                 .httpStatus(200)
                 .msg("등록 성공")
                 .result(null)
@@ -47,15 +46,15 @@ public class CenterController {
     @Operation(summary = "매장 수정")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공",
-                    content = {@Content(schema = @Schema(implementation = SampleResponseMessage.class))})
+                    content = {@Content(schema = @Schema(implementation = CenterResponseMessage.class))})
     })
     @PutMapping("{id}")
-    public ResponseEntity<?> putTest(@PathVariable("id") String id,
+    public ResponseEntity<CenterResponseMessage> putTest(@PathVariable("id") String id,
                                      @RequestBody CenterModifyRequestDTO centerModifyRequestDTO){
 
         centerCommandService.modifyCenter(id, centerModifyRequestDTO);
 
-        return ResponseEntity.ok(ResponseMessage.builder()
+        return ResponseEntity.ok(CenterResponseMessage.builder()
                 .httpStatus(200)
                 .msg("수정 성공")
                 .result(null)
@@ -65,14 +64,14 @@ public class CenterController {
     @Operation(summary = "매장 삭제")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공",
-                    content = {@Content(schema = @Schema(implementation = SampleResponseMessage.class))})
+                    content = {@Content(schema = @Schema(implementation = CenterResponseMessage.class))})
     })
     @DeleteMapping("{id}")
-    public ResponseEntity<?> deleteTest(@PathVariable("id") String id){
+    public ResponseEntity<CenterResponseMessage> deleteTest(@PathVariable("id") String id){
 
         centerCommandService.deleteCenter(id);
 
-        return ResponseEntity.ok(ResponseMessage.builder()
+        return ResponseEntity.ok(CenterResponseMessage.builder()
                 .httpStatus(200)
                 .msg("성공")
                 .result(null)
