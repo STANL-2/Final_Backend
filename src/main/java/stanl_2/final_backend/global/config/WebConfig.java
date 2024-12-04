d0bc1d282483ea6d0dff139811912656085403d5
minseokKim6823 <alstjr6823@naver.com>
2024-12-05 02:38:48 +0900

    cors error test중

--- src/main/java/stanl_2/final_backend/global/config/WebConfig.java
+++ src/main/java/stanl_2/final_backend/global/config/WebConfig.java
@@ -1,4 +1,5 @@
-import org.springframework.context.annotation.Bean;
+package stanl_2.final_backend.global.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
