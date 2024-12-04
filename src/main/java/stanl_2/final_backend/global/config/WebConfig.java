21d54b80c034e8b8be48bca2a8c004a6d43c1ad6
minseokKim6823 <alstjr6823@naver.com>
2024-12-05 02:56:07 +0900

    cors error test중

--- src/main/java/stanl_2/final_backend/global/config/WebConfig.java
+++ src/main/java/stanl_2/final_backend/global/config/WebConfig.java
@@ -1,5 +1,3 @@
-package stanl_2.final_backend.global.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,10 +7,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins("http://stanl2proj-env.eba-tupnyt2m.ap-northeast-2.elasticbeanstalk.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Content-Type", "Authorization")
-                .allowCredentials(true);
+        registry.addMapping("/api/**") // API 경로 매핑
+                .allowedOrigins("http://stanl2proj-env.eba-tupnyt2m.ap-northeast-2.elasticbeanstalk.com") // 클라이언트 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("Content-Type", "Authorization") // 허용할 요청 헤더
+                .exposedHeaders("Authorization") // 응답 헤더 노출
+                .allowCredentials(true) // 인증 정보 허용
+                .maxAge(3600); // Preflight 요청 캐싱 시간
     }
 }
