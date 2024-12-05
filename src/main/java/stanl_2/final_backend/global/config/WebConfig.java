e79b27b0e381a528836ad90d53c129f001e3b556
minseokKim6823 <alstjr6823@naver.com>
2024-12-06 02:05:01 +0900

    cors 에러 해결 도전

--- src/main/java/stanl_2/final_backend/global/config/WebConfig.java
+++ src/main/java/stanl_2/final_backend/global/config/WebConfig.java
@@ -9,8 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // API 경로 매핑
-                .allowedOrigins("http://stanl2proj-env.eba-tupnyt2m.ap-northeast-2.elasticbeanstalk.com") // 클라이언트 출처
+        registry.addMapping("/**") // API 경로 매핑
+                .allowedOrigins("https://stanl2-final-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("Content-Type", "Authorization") // 허용할 요청 헤더
                 .exposedHeaders("Authorization") // 응답 헤더 노출
